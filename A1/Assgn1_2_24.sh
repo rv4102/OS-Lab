d1bf921269441a2e2ec2dd3efc785002a318d6eb
Rushil Venkateswar <rushilv14@gmail.com>
2023-01-15 19:33:21 +0530

    edit

--- A1/Assgn1_2_24.sh
+++ A1/Assgn1_2_24.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 
 while IFS= read -r username; do
-    # check the username
     if [[ ${#username} -ge 5 ]] && [[ ${#username} -le 20 ]]; then
         if test -z "$(echo -n "$username" | tr -d 'a-zA-Z0-9')"; then
-            if [[ $(echo "$s" |grep -o "[0-9]" |grep -c "") -ne 0 ]]; then
-                if [[ ${str::1} =~ [a-zA-Z] ]]; then
+            if [[ $(echo "$username" |grep -o "[0-9]" |grep -c "") -ne 0 ]]; then
+                if [[ $username =~ ^[a-zA-Z] ]]; then
                     # check if username contains a substring mentioned in list of words in $2
-                else echo "NO" fi
-            else echo "NO" fi
-        else echo "NO" fi
-    else echo "NO" fi
-done
+                    
+                else 
+                    echo "NO" 
+                fi
+            else 
+                echo "NO" 
+            fi
+        else 
+            echo "NO" 
+        fi
+    else 
+        echo "NO" 
+    fi
+done < $1
