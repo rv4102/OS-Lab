f7c15fa4d9e6646667b4e711163de7462fff2d5e
Shivam751 <77993136+Shivam751@users.noreply.github.com>
2023-01-16 19:12:16 +0530

    Update Assgn1_9_24.sh

--- A1/Q9/Assgn1_9_24.sh
+++ A1/Q9/Assgn1_9_24.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
-
+filename=$1
 sed 's/.* //' $1 | uniq -c | sed 's/\([0-9]\)/-\1/' | sort -t ' ' -k2 | sort -t ' ' -nk1 | awk '{printf "%s %d\n", $2, -$1}'
+awk '
+{
+    students[$1] += 1
+}
+END{
+    single_course_stus = 0
+    for (i in students){
+        if (students[i] > 1){
+            print i
+        }
+        if(students[i] == 1){
+            single_course_stus += 1
+        }
+    }
+    print single_course_stus
+}' $filename
