417416f47a4d3509dc3430d105a00d9574da363a
Rushil Venkateswar <rushilv14@gmail.com>
2023-01-16 19:13:48 +0530

    edit

--- A1/Q2/Assgn1_2_24.sh
+++ A1/Q2/Assgn1_2_24.sh
@@ -2,7 +2,7 @@
 
 while IFS= read -r username; do
     flag=0
-    if [[ ${#username} -ge 5 ]] && [[ ${#username} -le 20 ]] && test -z "$(echo -n "$username" | tr -d 'a-zA-Z0-9')" && [[ $(echo "$username" |grep -o "[0-9]" |grep -c "") -ne 0 ]] && [[ $username =~ ^[a-zA-Z] ]]; then
+    if [[ ${#username} -ge 5 ]] && [[ ${#username} -le 20 ]] && [[ $username =~ ^[a-zA-Z][a-zA-Z0-9]*[0-9][a-zA-Z0-9]*$ ]]; then
         while IFS= read -r fruitname; do
             if [[ $(echo -n "$username" | grep -iFc "$fruitname") -ne 0 ]]; then
                 flag=1
