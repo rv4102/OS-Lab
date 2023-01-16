471eb09b40c14451781fddc6c0dfe870575375ae
Rushil Venkateswar <rushilv14@gmail.com>
2023-01-16 23:46:57 +0530

    Q4 done

--- A1/Q4/Assgn1_4_24.sh
+++ A1/Q4/Assgn1_4_24.sh
@@ -5,7 +5,7 @@ while read line;do
     found=0
     echo $line | grep -q $input_word && found=1
     if [ $found -eq 1 ];then
-        
+        echo "$line" | tr '[:lower:]' '[:upper:]' | sed -e:a -e 's/\([A-Z][^a-zA-Z]*\)\([A-Z]\)/\1\l\2/;ta'
     else
         echo $line
     fi
