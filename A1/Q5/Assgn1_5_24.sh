7cac39283bd6d54029132412079c074b4ab20d3e
Rushil Venkateswar <rushilv14@gmail.com>
2023-01-16 20:23:47 +0530

    Q5 done

--- A1/Q5/Assgn1_5_24.sh
+++ A1/Q5/Assgn1_5_24.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 for f in $(find "$1" -name '*.py'); do 
-    file=echo $f|tr "/" "\n"|tail -1
-    echo "Name: $file"
+    file=$(echo "$f"|tr "/" "\n"|tail -1)
+    echo "\nName: $file"
     echo "Path: $f"
-    
+    awk '/#/ {print NR". "$0} /\n/' < $f
+    awk '/"""/ {print NR". "} /"""/' < $f
 done
