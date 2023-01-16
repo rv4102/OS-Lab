645869883c378e844fdc1919fa1ca5b03d7aa091
Rushil Venkateswar <rushilv14@gmail.com>
2023-01-16 13:18:16 +0530

    optimize

--- A1/Q2/Assgn1_2_24.sh
+++ A1/Q2/Assgn1_2_24.sh
@@ -2,26 +2,13 @@
 
 while IFS= read -r username; do
     flag=0
-    if [[ ${#username} -ge 5 ]] && [[ ${#username} -le 20 ]]; then
-        if test -z "$(echo -n "$username" | tr -d 'a-zA-Z0-9')"; then
-            if [[ $(echo "$username" |grep -o "[0-9]" |grep -c "") -ne 0 ]]; then
-                if [[ $username =~ ^[a-zA-Z] ]]; then
-                    # check if username contains a substring mentioned in list of words in $2
-                    while IFS= read -r fruitname; do
-                        if [[ $(echo -n "$username" | grep -iFc "$fruitname") -ne 0 ]]; then
-                            flag=1
-                            break
-                        fi
-                    done < $2 
-                else
-                    flag=1
-                fi
-            else
+    if [[ ${#username} -ge 5 ]] && [[ ${#username} -le 20 ]] && test -z "$(echo -n "$username" | tr -d 'a-zA-Z0-9')" && [[ $(echo "$username" |grep -o "[0-9]" |grep -c "") -ne 0 ]] && [[ $username =~ ^[a-zA-Z] ]]; then
+        while IFS= read -r fruitname; do
+            if [[ $(echo -n "$username" | grep -iFc "$fruitname") -ne 0 ]]; then
                 flag=1
+                break
             fi
-        else
-            flag=1
-        fi
+        done < $2 
     else
         flag=1
     fi
