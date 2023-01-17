109c2ead15d3bd8b8cf45c70bff374bb0fc5114d
Rushil Venkateswar <rushilv14@gmail.com>
2023-01-17 09:44:23 +0530

    issue with docstrings

--- A1/Q5/Assgn1_5_24.sh
+++ A1/Q5/Assgn1_5_24.sh
@@ -4,6 +4,6 @@ for f in $(find "$1" -name '*.py'); do
     file=$(echo "$f"|tr "/" "\n"|tail -1)
     echo "\nName: $file"
     echo "Path: $f"
-    awk '/#/ {print NR". "$0} /\n/' < $f
-    awk '/"""/ {print NR". "} /"""/' < $f
+    grep -Eon '^ *#.*' $f
+    grep -Eon '^ *""".*?' $f
 done
