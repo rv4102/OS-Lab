711bb719fbf20299d9f966720234d7bfd2fe9a61
Rushil Venkateswar <rushilv14@gmail.com>
2023-01-17 10:18:48 +0530

    add line numbers

--- A1/Q5/Assgn1_5_24.sh
+++ A1/Q5/Assgn1_5_24.sh
@@ -2,8 +2,8 @@
 
 for f in $(find "$1" -name '*.py'); do 
     file=$(echo "$f"|tr "/" "\n"|tail -1)
-    echo "\nName: $file"
-    echo "Path: $f"
+    echo "\nName: $file \nPath: $f"
     grep -Eon '^ *#.*' $f
-    grep -Eon '^ *""".*?' $f
+    sed -n '/"""/,/"""/p' $f
+    sed -n "/'''/,/'''/p" $f
 done
