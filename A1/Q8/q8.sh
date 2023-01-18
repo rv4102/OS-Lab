2cba77fdab03077f34b3cb46df3f466d2cedab1d
Shivam751 <77993136+Shivam751@users.noreply.github.com>
2023-01-18 14:26:30 +0530

    Update q8.sh

--- A1/Q8/q8.sh
+++ A1/Q8/q8.sh
@@ -1,20 +1,12 @@
 #!/bin/bash
 filename=main.csv
-if [ -f $filename ]; then
-    echo "$filename already exists."
-else
+if [ ! -f $filename ]; then
     touch main.csv
-    echo "Date,Category,Amount,Name" >> main.csv
 fi
-echo "$1,$2,$3,$4" >> main.csv
-echo "Inserted $1,$2,$3,$4 into main.csv"
-
 while getopts ":c:n:s:h" opt; do
     case $opt in
     c)
-        echo "option c was triggered, argument: $OPTARG"
-        category=$OPTARG
-        awk 'BEGIN {
+        awk -v category=$OPTARG 'BEGIN {
             FS=","
             amount=0
         }
@@ -24,21 +16,51 @@ while getopts ":c:n:s:h" opt; do
             }
         }
         END {
-            print amount
-        }' 
-
+            print "Total amount spent in " category " is " amount
+        }' main.csv
         ;;
-    n)
-        name=$OPTARG
+    n)  
+        awk -v name=$OPTARG 'BEGIN {
+            FS=","
+            amount=0
+        }
+        {
+            if($4 == name) {
+                amount+=$3
+            }
+        }
+        END {
+            print "Total amount spent by " name " is " amount
+        }' main.csv
         ;;
     s)
-        #sort csv by column name
+        col=1
+        case $OPTARG in
+        Date)
+            col=1
+            ;;
+        Category)
+            col=2
+            ;;
+        Amount)
+            col=3
+            ;;
+        Name)
+            col=4
+            ;;
+        esac
+        sort -t, -k$col main.csv -o main.csv 
         ;;
     h)
         echo "NAME\n\tq8.sh - a script to track expenses\nSYNOPSIS\n\tq8.sh [OPTION]... RECORD...\nDESCRIPTION\n\tCreate the csv file main.csv if it does not exist and insert the record into the file.\n\t-c, =Category: accepts a category and prints the amount of money spend in that category\n\t-n, =Name: accepts a name and print the amount of money spent by that person\n\t-s, =Column: accepts a column name sort the csv file by the column name\n\t-h, =help: show help prompt"
         ;;
     \?)
-        echo "Invalid option: -$OPTARG" >&2
+        echo "Invalid option: -$OPTARG" 
         ;;
     esac
 done
+
+shift $((OPTIND-1))
+if [ $# -eq 4 ]; then
+    echo $1,$2,$3,$4 >> main.csv
+fi
