699bef08aa56b470ae068a79acae2d6fc8d4bcf4
Rushil Venkateswar <rushilv14@gmail.com>
2023-01-23 02:39:24 +0530

    26 chars

--- A1/Q4/Assgn1_4_24.sh
+++ A1/Q4/Assgn1_4_24.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-while read line;do
-    found=0
-    echo $line | grep -q $2 && found=1
-    if [ $found -eq 1 ];then
-        echo "$line" | tr '[:lower:]' '[:upper:]' | sed -e:a -e 's/\([A-Z][^a-zA-Z]*\)\([A-Z]\)/\1\l\2/;ta'
+while read line; do
+    if [[ $line =~ "$2" ]]; then
+        echo "${line^^}" | sed -e:a -e 's/\([A-Z][^a-zA-Z]*\)\([A-Z]\)/\1\l\2/;ta'
     else
         echo "$line"
     fi
