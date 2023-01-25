73bede07d88e1fb763ad6191fe5fdb5884d58e65
Rushil Venkateswar <rushilv14@gmail.com>
2023-01-25 08:20:37 +0530

    final changes

--- A1/Q6/Assgn1_6_24.sh
+++ A1/Q6/Assgn1_6_24.sh
@@ -1,43 +1,28 @@
 #!/bin/bash
-input_file=$1
 maxn=1000000
 declare -a spf
-sieve_of_eratosthene() {
-    for((i=1; i<=maxn; i++))
-    do
-        spf[i]=$i
-    done
-    for((i=4; i<=maxn; i+=2))
-    do
-        spf[i]=2
-    done
-    for((p=3; p*p<=maxn; p=p+2))
-    do 
-        if [[ ${spf[p]} -eq $p ]]; then
-            for((i=p*p; i<=maxn; i+=p))
-            do
-                if [[ ${spf[i]} -eq $i ]]; then
-                    spf[i]=$p
-                fi
-            done   
-        fi 
-    done
-}
-sieve_of_eratosthene
-output_path=output.txt
-while read -r n
+for ((i=1; i<=maxn; i++))
 do
-    for((i=1; i<=n; i++))
-    do
-        while [ $n -ne 1 ]
+    spf[$i]=$i
+done
+for((i=2; i*i<=maxn; i++))
+do
+    if [ ${spf[$i]} -eq $i ]
+    then
+        for((j=i*i; j<=maxn; j+=i))
         do
-            factor=${spf[n]}
-            while [[ $(($n%$factor)) -eq 0 ]]
-            do
-                n=$((n/factor))
-            done
-            echo -n "$factor "
+            if [ ${spf[$j]} -eq $j ]
+            then
+                spf[$j]=$i
+            fi
         done
+    fi
+done
+>output.txt
+while read n; do
+    while [[ $n -ne 1 ]]; do
+        echo -n "${spf[$n]} "
+        n=$((n/${spf[$n]}))
     done
     echo
-done < $input_file > $output_path
+done < $1
