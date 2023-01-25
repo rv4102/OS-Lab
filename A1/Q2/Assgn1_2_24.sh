fc0c61568fc03b8c5906d8caadc30fc288730a72
Kushaz Sehgal <kushaz.sehgal@gmail.com>
2023-01-25 09:48:59 +0530

    reduced Q2

--- A1/Q2/Assgn1_2_24.sh
+++ A1/Q2/Assgn1_2_24.sh
@@ -1,15 +1,9 @@
 #!/bin/bash
 
-while read -r username; do
+while read  username; do
     out=NO
-    if (( 5 <= ${#username} <= 20 )) && [[ $username =~ ^[a-zA-Z][a-zA-Z0-9]*[0-9][a-zA-Z0-9]*$ ]]; then
-        out=YES
-        while read -r fruitname; do
-            if [[ "${username,,}" == *"${fruitname,,}"* ]]; then
-                out=NO
-                break
-            fi
-        done < "fruits.txt"
+    if echo ${username} | grep -qP "^[a-zA-Z](?=.*[0-9])[a-zA-Z0-9]{4,19}$" &&  ! grep -qif fruits.txt <<< ${username} ;then
+        out=YES   
     fi
     echo $out >> "validation_results.txt"
 done < $1
