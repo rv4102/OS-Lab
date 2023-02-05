7b43a42358152f7d440c2aa54107679c58488e70
Shivam751 <77993136+Shivam751@users.noreply.github.com>
2023-02-05 23:52:25 +0530

    Update try.cpp

--- A1/Q2/Assgn1_2_24.sh
+++ A1/Q2/Assgn1_2_24.sh
@@ -3,6 +3,8 @@ while read  username; do
     out=NO
     if echo ${username} | grep -qP "^[a-zA-Z](?=.*[0-9])[a-zA-Z0-9]{4,19}$" &&  ! grep -qif fruits.txt <<< ${username} ;then
         out=YES   
+    if echo ${username} | grep -qP "^[a-zA-Z](?=.*[0-9])[a-zA-Z0-9]{4,19}$" &&  ! grep -qif fruits.txt <<< ${username} ;then
+        out=YES   
     fi
     echo $out >> "validation_results.txt"
 done < $1
